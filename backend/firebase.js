60a99f8f46c2d0291c2572cb54b20f829a74aad3
GannuAbhijithsai <96736183+GannuAbhijithsai@users.noreply.github.com>
2024-07-08 16:03:30 +0530

    Update firebase.js

--- backend/firebase.js
+++ backend/firebase.js
@@ -3,7 +3,7 @@ const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "gs://social-media-ff271.appspot.com",
+  storageBucket: "social-media-ff271.appspot.com",
 });
 
 const bucket = admin.storage().bucket();
